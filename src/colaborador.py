2792b31317c4a7fc2a01121eecef4425f58f4380
Aguilar-2005 <luis.aguilar2708@outlook.com>
2025-05-27 00:07:45 -0500

    commit

--- src/colaborador.py
+++ src/colaborador.py
@@ -1,9 +1,11 @@
-from src.miembro_equipo import MiembroEquipo
-
+#Importamos la clase base MiembroEquipo desde el módulo correspondiente
+from src.miembro_equipo import MiembroEquipo 
+#Definimos la clase Colaborador que hereda de MiembroEquipo
 class Colaborador(MiembroEquipo):
     """
     Subclase que representa a un colaborador.
     Miembros del grupo: Grupo 3 (Aguilar Luis, Guzman Keyla, Moran Leonardo, Almazan Nayra)
     """
+     #Método que permite asignar una tarea al colaborador
     def asignar_tarea(self, tarea):
         print(f"{self.nombre} (Colaborador) acepta la tarea: {tarea.descripcion}")
