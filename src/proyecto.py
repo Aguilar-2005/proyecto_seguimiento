a084addf9f43098721f10a564772ddff6c929518
Aguilar-2005 <luis.aguilar2708@outlook.com>
2025-05-26 23:51:40 -0500

    se agrego comentario

--- src/proyecto.py
+++ src/proyecto.py
@@ -4,14 +4,18 @@ class Proyecto:
     Miembros del grupo: Grupo 3 (Aguilar Luis, Guzman Keyla, Moran Leonardo, Almazan Nayra)
     """
     def __init__(self, nombre):
+        #Inicializamos el proyecto con un nombre, y listas vacías para tareas y miembros del equipo
         self.nombre = nombre
-        self.tareas = []
-        self.equipo = []
+        self.tareas = [] #Creamos lista para almacenar las tareas del proyecto
+        self.equipo = [] #Creamos lista para almacenar los miembros del equipo
     def agregar_tarea(self, tarea):
+         #Agregamos un objeto de tipo tarea a la lista de tareas
         self.tareas.append(tarea)
     def agregar_miembro(self, miembro):
+        #Agregamos un objeto de tipo miembro a la lista del equipo
         self.equipo.append(miembro)
     def mostrar_info(self):
+         #Mostramos la información general del proyecto, incluyendo tareas y equipo
         print(f"Proyecto: {self.nombre}")
         print("Tareas:")
         for tarea in self.tareas:
