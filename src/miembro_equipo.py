dcd0ffd9431cf99cd20e994eab5c8a9d8239b0a5
Aguilar-2005 <luis.aguilar2708@outlook.com>
2025-05-27 00:10:28 -0500

    edicion comentario

--- src/miembro_equipo.py
+++ src/miembro_equipo.py
@@ -1,10 +1,11 @@
+#Definimos la clase base para representar a un miembro del equipo
 class MiembroEquipo:
     """
     Clase base para representar a un miembro del equipo.
     Miembros del grupo: Grupo 3 (Aguilar Luis, Guzman Keyla, Moran Leonardo, Almazan Nayra)
     """
     def __init__(self, nombre):
-        #Asignamos el parámetro nombre al atributo de instancia del mismo nombre
+        #Constructor de la clase que inicializa el atributo nombre con el valor proporcionado
         self.nombre = nombre
     def asignar_tarea(self, tarea):
         """
