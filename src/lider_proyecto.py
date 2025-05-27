39af9d8a0c5c244b31618a11483c1b5b8af80177
Aguilar-2005 <luis.aguilar2708@outlook.com>
2025-05-27 00:08:59 -0500

    commit

--- src/lider_proyecto.py
+++ src/lider_proyecto.py
@@ -1,8 +1,11 @@
+#Importamos la clase base MiembroEquipo desde el módulo correspondiente
 from src.miembro_equipo import MiembroEquipo
+#Definimos la clase LiderProyecto, que hereda de la clase MiembroEquipo
 class LiderProyecto(MiembroEquipo):
     """
     Subclase que representa al líder del proyecto.
     Miembros del grupo: Grupo 3 (Aguilar Luis, Guzman Keyla, Moran Leonardo, Almazan Nayra)
     """
+    #Método que permite al líder asignar una tarea
     def asignar_tarea(self, tarea):
         print(f"{self.nombre} (Líder) asigna la tarea: {tarea.descripcion}")
